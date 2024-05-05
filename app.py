df45202e8732a030239011f5e77cffbda7a3f756
mohamed682004 <modyhamdyatahossen@gmail.com>
2024-05-05 14:35:23 +0300

    Add Docker and requirements files

--- app.py
+++ app.py
@@ -1,19 +1,30 @@
 from flask import Flask, render_template
+import pymysql
 
 app = Flask(__name__)
 
+# MySQL Configuration
+config = {
+  'user': 'root',
+  'password': '2004',
+  'host': 'localhost',
+  'database': 'cloudy_members',
+  'port': 3306,
+  'cursorclass': pymysql.cursors.DictCursor
+}
 
-# Not implemented
+cnx = pymysql.connect(**config)
+
 @app.route("/")
 def home():
     return render_template("index.html")
 
-
-# Not implemented
 @app.route("/team")
 def team():
-    return render_template("team.html")
-
+    with cnx.cursor() as cursor:
+        cursor.execute("SELECT * FROM team")
+        data = cursor.fetchall()
+    return render_template("team.html", team=data)
 
 if __name__ == "__main__":
     app.run(debug=True)
